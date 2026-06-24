d981eaed28102d9256ece0cd9c2689a6be26d488
feng <dujiafeng@mail.com>
2026-06-24 15:43:37 +0800

    添加 Embedding 客户端管理 (OpenAIEmbeddings 封装)

---
+++ app/clients/embedding_client_manager.py
@@ -0,0 +1,36 @@
+import asyncio
+
+from langchain_openai import OpenAIEmbeddings
+
+from app.conf.app_config import EmbeddingConfig, app_config
+
+
+class EmbeddingClientManager:
+    def __init__(self, config: EmbeddingConfig):
+        self.client: OpenAIEmbeddings | None = None
+        self.config = config
+
+    def _get_url(self):
+        return f"http://{self.config.host}:{self.config.port}"
+
+    def init(self):
+        self.client = OpenAIEmbeddings(
+            base_url=self._get_url(),
+            model=self.config.model,
+            api_key="not-needed")
+
+
+embedding_client_manager = EmbeddingClientManager(app_config.embedding)
+
+if __name__ == '__main__':
+    embedding_client_manager.init()
+    client = embedding_client_manager.client
+
+
+    async def test():
+        text = "What is deep learning?"
+        query_result = await client.aembed_query(text)
+        print(query_result[:3])
+
+
+    asyncio.run(test())
